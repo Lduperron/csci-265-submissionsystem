3e921837d9b03edefa06f62eda968d603bff0d7d
meara.kimball@gmail.com <meara.kimball@gmail.com@98db6ee2-f8d6-e7b7-daac-2c9c925a93f8>
2012-10-29 17:40:24 +0000

    at least it compiles

--- ModuleImplementations/SubmitServer.pl
+++ ModuleImplementations/SubmitServer.pl
@@ -1,74 +1,95 @@
 #!/usr/bin/perl
 
+$| = 1;
+
 use IO::File;
 use IO::Socket;
-use lib '/265/asgm12.csci-265-submissionsysytem/ModuleImplementations/PasswordGen';
+use lib '/265/asgm12/csci-265-submissionsystem/ModuleImplentations/PasswordGen/PassMod.pm';
 use strict;
 use warnings;
 
-my $sock = new IO::Socket::INET (                  #fill in values later
+my $sock = new IO::Socket::INET (
                               LocalHost => '',
                               LocalPort => '7071',
                               Proto => 'tcp',
-			                     Listen => 3,
+			                  Listen => 3,
                               Reuse => 1
                               );
+die "Error: Unable to create socket: $!\n" unless $sock;
 
-die "Could not create socket: $!\n" unless $sock;
-
-my @error = (0, 1, 2, 3, 4);
 my $root = "../../config/";
 my $studentPath = $root . "StudentConfig.txt";
 my $coursesPath = $root . "courses/";
+my $coursePath;
+my $course;
+my $user;
+my $password;
+while(my $new_sock = $sock->accept()) {     
+    for (my $i = 0; $i < 4; $i++) {        
+        my $line = <$new_sock>;        
+        if(defined($line)) {            
+            if ($i == 0) {
+                chop $line;     
+                ($user, $password, $course) = split(":", $line);
+                
+                if (!verify($user, $password)) { #Check password
+                    print $new_sock '0';    #user or password is incorrect
+                    close($new_sock);
+                    $i = 4;
+                } elsif (!isEnrolled($studentPath, $user, $course)) {
+                    print $new_sock '1';    #user isn't in that course
+                    $i = 4;                
+                }
 
-while (my $new_sock = $sock->accept()) {
-   my $line=<$new_sock>;
-   chop $line;
-   (my $user, my $password, my $course, my $fileName) = split(":", $line);
+            } elsif ($i == 1 || $i == 2) {
+                chop $line;    # name of assignment
+                my $length = $line;
+                chop $line;
+                my @asgm = split (".", $line);
+                $coursePath = $coursesPath . $course;            
+                if (!validAsgm ($coursePath, $line)) {
+                    print $new_sock '2';
+                    close($new_sock);
+                    $i = 4;
+                } 
+                my $asgmPath = $coursePath . $asgm[0] . "/". $asgm[0] . ".txt";    
+                    #asgm.txt stored in asgm directory
+                my @serverTime = split(" ", localtime);
+                my $subDate = dateTime( $serverTime[4] , $serverTime[1] , $serverTime[2]);
    
-   
-   if (!Verify($user, $password)) { #Check password
-      print $new_sock $error[0];    #user or password is incorrect
-      close($new_sock);
-   }
-                                    #otherwise, student exists
-   
-   #check student exists - find name in master enrollment
-   
-   #if (!isInFile($studentPath, $user) {
-   #   print $new_sock $error[0];
-   #   close($new_sock);
-   #}      
-   
-
-   #check enrollment - check that course is after student name
-   
-   #2
-   if (!isEnrolled($studentPath, $user, $course)) {
-      print $new_sock $error[1]
-   }
-
-   #check assignment - name of file exists in that class
-   my $coursePath = $coursesPath . $course;
-   if (!validAsgm ($coursePath, $fileName)) {
-      print $new_sock $error[2];
-      close($new_sock);
-   }
-   
-   #check due date - server time vs assignment due date
-   my $asgmPath = $coursePath . my $asgm . ".txt";
-   my @serverTime = split(" ", localtime);
-   my $subDate = dateTime( $serverTime[4] , $serverTime[1] , $serverTime[2]);
-   
-   if (!onTime( $subDate, $asgmPath)) {
-      print $new_sock $error[3];
-      close($new_sock);
-   }
-   
-   
-   
-   #store file
+                if (!onTime( $subDate, $asgmPath)) {
+                    print $new_sock '3';
+                    close($new_sock);
+                    $i = 4;
+                }
+                else {
+                    #make txt file in correct directory & store socket file in it
+                    # with user file already in assignment dir
+                    my $filePath = $coursePath . $asgm[0] . "/" . $user;
+                    open(my $submsn, '<', $filePath . $asgm[0] . ".txt");
+                    my $j = 0;
+                    while (defined($line)) {
+                        print $submsn $line;
+                        $line = <$new_sock>;
+                        $j++;
+                    }
+                    if ($j != $length){
+                    print $new_sock "4"; #file was unexpected length
+                    close($new_sock);
+                    unlink($submsn);    
+                    }else{
+                    print $new_sock "5";
+                    close ($new_sock);
 }
+                    $i = 4;
+                }
+            }             
+        }
+    }
+} 
+   
+close($sock);
+
 sub isEnrolled #path, user, course
 {
 my $path = shift @_;
@@ -116,7 +137,8 @@ sub onFileLine # filePath, searchTerm
    
 }
 
-sub findAsgm # coursepath, assignment name
+sub validAsgm # coursepath, assignment name
+              # opens course directory, checks if assignment exists
 {
    my $coursePath = shift @_;
    my $asgm = shift @_;
@@ -137,6 +159,8 @@ sub findAsgm # coursepath, assignment name
 }
 
 sub onTime # checks if file is on time
+           # compares date it was submitted with due date
+           #if sub<=due, it's good
 {
    my $subDate = shift @_;
    my $path = shift @_;
@@ -154,7 +178,8 @@ sub onTime # checks if file is on time
    return $onTime;
 }
 
-sub dateTime #takes year, month, day & binds into 1 variable
+sub dateTime # takes year, month, day & binds into 1 variable
+             # in same format of due date
 {
    my $year = shift @_;
    my $month = shift @_;
@@ -193,3 +218,4 @@ sub dateTime #takes year, month, day & binds into 1 variable
    $finalTime = $finalTime . $day;
    return $finalTime;
 }
+
