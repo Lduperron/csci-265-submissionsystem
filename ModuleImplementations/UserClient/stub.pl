3061186aef026d42d463b9c9d2f7fb9723806f28
oliver@harpcanada.com <oliver@harpcanada.com@98db6ee2-f8d6-e7b7-daac-2c9c925a93f8>
2012-11-01 02:17:09 +0000

    Quick update on UserClient...

--- ModuleImplementations/UserClient/stub.pl
+++ ModuleImplementations/UserClient/stub.pl
@@ -6,10 +6,10 @@ $| = 1;
 use IO::Socket;
 
 $sock = new IO::Socket::INET (
-                              LocalHost => '',
-                              LocalPort => '8585',
-                              Proto => 'tcp',
-			      Listen => 3,
+                              LocalHost => "",
+                              LocalPort => "6709",
+                              Proto => "tcp",
+			                     Listen => 3,
                               Reuse => 1
                               );
 die "Error: Unable to create socket: $!\n" unless $sock;
@@ -18,33 +18,42 @@ while($new_sock = $sock->accept()) {
 	for ($i = 0; $i < 2; $i++) {	 
 	 	$line = <$new_sock>;        
      	if(defined($line)) {            
-     	    chop $line;
+     	    chomp $line;
 			if ($i == 0) {   
      	        ($name, $passw, $class) = split(":", $line);
-     	        if ($name ne 'matthew') { 
-     	            print $new_sock '0';
-     	            print "CLOSE\n"; 
+     	        if ($name ne "matthew") { 
+     	            print $new_sock "0\n";
+     	            print "name\n"; 
      	            close($new_sock);
      	            $i = 4; 
-     	        } elsif ($passw ne 'math') {
-     	            print $new_sock '0';
-     	            print "CLOSE\n"; 
+     	        } elsif ($passw ne "math") {
+     	            print $new_sock "0\n";
+     	            print "pass\n"; 
      	            close($new_sock);
      	            $i = 4;
-     	        } elsif ($class ne 'top') {
-    	 	            print $new_sock '1';
-     	            print "CLOSE\n"; 
+     	        } elsif ($class ne "top") {
+    	 	         print $new_sock "1\n";
+     	            print "class\n"; 
      	            close($new_sock);
      	            $i = 4;
-     	        }
+     	        }else{
+                 print $new_sock "400\n";      
+ 
+                 print "all good\n";
+              }
 		 	 } elsif ($i == 1) {
-			  	if ($line ne 'file.txt') { 
-                    print $new_sock '2';
+			  	if ($line ne "file.txt") { 
+                    print $new_sock "2\n";
                     print "CLOSE\n"; 
                     close($new_sock);
                     $i = 4;
-                } 
-			}
+                } else{
+                 print $new_sock "400\n";        
+              }
+            }
+
+  
+
 		} 
 	} 
 
@@ -55,13 +64,14 @@ while($new_sock = $sock->accept()) {
 		for ($i = 0; $i < $num; $i++) {        
     			chop $line;                
         		if (defined($line)) {	
-				if ($line eq '1') {
-	        		    print $new_sock '5';
+				if ($line eq "1") {
+	        		    print $new_sock "5\n";
 	        		    print "CLOSE\n"; 
 	        		    close($new_sock);
-	        		    $i = $num;
+                   last;
+	        		    
 	        		} else {
-	        		    print $new_sock '4';
+	        		    print $new_sock "4\n";
 	        		    print "CLOSE\n"; 
 	        		    close($new_sock);
 	        		    $i = $num;
