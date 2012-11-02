e13861c2b364ce125849a54bc33a488107b50341
matthewcormons@gmail.com <matthewcormons@gmail.com@98db6ee2-f8d6-e7b7-daac-2c9c925a93f8>
2012-11-02 18:26:07 +0000

    Changed port

--- ModuleImplementations/UserClient/stub2.pl
+++ ModuleImplementations/UserClient/stub2.pl
@@ -7,7 +7,7 @@ use IO::Socket;
 
 $sock = new IO::Socket::INET (
                               LocalHost => '',
-                              LocalPort => '9001',
+                              LocalPort => '8585',
                               Proto => 'tcp',
 			      Listen => 3,
                               Reuse => 1
